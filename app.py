19598db2eeba68bfec6858e88bc7fd6d62dc7339
Damien Ayers <damien@omad.net>
2019-04-12 17:08:16 +1000

    Simple DEA Developer Dashboard

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+repositories = [
+    {'user': 'GeoscienceAustralia', 'repo': 'fc'},
+    {'user': 'GeoscienceAustralia', 'repo': 'wofs'},
+    {'user': 'GeoscienceAustralia', 'repo': 'digitalearthau',
+     },
+    {'user': 'GeoscienceAustralia', 'repo': 'dea-orchestration',
+     },
+    {'user': 'opendatacube', 'repo': 'datacube-core',
+     'rtd_name': 'datacube-core', 'codecov': True},
+    {'user': 'opendatacube', 'repo': 'datacube-ows'},
+    {'user': 'opendatacube', 'repo': 'datacube-stats'},
+    {'user': 'GeoscienceAustralia', 'repo': 'wagl'},
+    {'user': 'GeoscienceAustralia', 'repo': 'eo-datasets', 'coveralls': True},
+    {'user': 'GeoscienceAustralia', 'repo': 'COG-Conversion'},
+    {'user': 'opendatacube', 'repo': 'datacube-charts'},
+    {'user': 'GeoscienceAustralia', 'repo': 'dea-config'},
+]
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', repos=repositories)
+
+
+if __name__ == '__main__':
+    app.run()
